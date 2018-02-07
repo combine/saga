afff083efaad926f64f837e65e0238ec6aa7b631
Calvin Lai <callai@gmail.com>
2018-02-07 00:27:07 -0500

    feat: slugify product names

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,6 +1,9 @@
 import Base from './Base';
+import { Slugify } from 'objection-slugify';
 
-export default class Product extends Base {
+const SluggedModel = Slugify(Base, { sourceField: 'name', unique: true });
+
+export default class Product extends SluggedModel {
   static tableName = 'products';
   static jsonSchema = {
     type: 'object',
@@ -9,6 +12,7 @@ export default class Product extends Base {
     properties: {
       id: { type: 'integer' },
       name: { type: 'string', minLength: 1, maxLength: 255 },
+      slug: { type: 'string', minLength: 1, maxLength: 255 },
       description: { type: ['string', 'null'] }
     }
   };
