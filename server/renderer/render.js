d3caab2714c45e9995fa8abdd1f8578c25b47cc2
Calvin Lai <callai@gmail.com>
2018-06-21 02:29:58 -0400

    feat: cleaner asset manifest builder

--- server/renderer/render.js
+++ server/renderer/render.js
@@ -4,26 +4,43 @@ import { Helmet } from 'react-helmet';
 import { manifestFilename } from '@config';
 import { safeRequire } from './helpers';
 
-export default async function render(
-  html,
-  layout,
-  initialState = {},
-  bundles = []
-) {
-  const outputPath = process.env.PUBLIC_OUTPUT_PATH || 'dist/public';
-  const module = path.join(__dirname, '..', '..', outputPath, manifestFilename);
-  const assets = safeRequire(module);
+const output = process.env.PUBLIC_OUTPUT_PATH || 'dist/public';
+const manifestPath = path.join(__dirname, '../..', output, manifestFilename);
+
+const getAssets = (layout, chunks = []) => {
+  const manifest = safeRequire(manifestPath);
+  const isLayout = f => f.match(new RegExp(layout));
+  const isVendor = f => f.match(/vendor/);
+  const isCss = f => f.match(/.css/);
+  const isJs = f => f.match(/.js/);
+  const assetPath = k => manifest[k];
+
+  const keys = Object.keys(manifest);
+
+  // vendors go first.
+  const bundles = [ ...keys.filter(isVendor), ...keys.filter(isLayout) ];
+
+  return {
+    css: [
+      ...bundles.filter(isCss).map(assetPath),
+      ...chunks.map(b => b.file).filter(isCss)
+    ],
+    js: [
+      ...bundles.filter(isJs).map(assetPath),
+      ...chunks.map(b => b.file).filter(isJs)
+    ]
+  };
+};
+
+export default async function render(html, layout, state = {}, chunks = []) {
+  const assets = getAssets(layout, chunks);
   const compile = template(safeRequire(`@templates/layouts/${layout}.html`));
   const helmet = Helmet.renderStatic();
-  const chunkCss = bundles.filter(bundle => bundle.file.match(/.css/));
-  const chunkJs = bundles.filter(bundle => bundle.file.match(/.js/));
 
   return compile({
     html,
     helmet,
     assets,
-    chunkCss,
-    chunkJs,
-    initialState
+    state
   });
 }
